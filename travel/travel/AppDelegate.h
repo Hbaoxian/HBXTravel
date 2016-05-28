190e0ba3fee70ebafa3d72f57091b1c98d0fb4ce
黄保贤 <huangbaoxian@edaijia-inc.cn>
2016-05-28 09:53:02 +0800

    创建工程

---
+++ travel/travel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  travel
+//
+//  Created by 黄保贤 on 16/5/28.
+//  Copyright © 2016年 黄保贤. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
